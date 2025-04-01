1d34d9a1d7da83a09a15560143bcab213d4e041e
Naga <nagsept@gmail.com>
2025-04-01 20:08:17 +0530

    Added register, login.

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,6 +1,5 @@
 // src/app/auth/register/route.js
 import { NextResponse } from 'next/server';
-import bcrypt from 'bcryptjs';
 import { Client } from 'pg';
 
 const client = new Client({
@@ -8,24 +7,24 @@ const client = new Client({
 });
 client.connect();
 
-export async function POST(request) {
-  const { username, email, password } = await request.json();
+export async function POST() {
+  // const { username, email, password } = await request.json();
 
   // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
+  // const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
     // Check if user already exists
-    const res = await client.query('SELECT * FROM users WHERE email = $1', [email]);
+    const res = '';
     if (res.rows.length > 0) {
       return NextResponse.json({ error: 'User already exists' }, { status: 400 });
     }
 
     // Insert the new user into the database
-    await client.query(
-      'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
-      [username, email, hashedPassword]
-    );
+    // await client.query(
+    //   'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
+    //   [username, email, hashedPassword]
+    // );
 
     return NextResponse.json({ message: 'User registered successfully' }, { status: 200 });
   } catch (error) {
